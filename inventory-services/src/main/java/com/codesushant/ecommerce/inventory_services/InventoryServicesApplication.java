727f9d72ac813243488640f4f0cf222369760bb3
Sushant Bait <sushantbait93@gmail.com>
2026-02-09 21:05:37 +0530

    Create FeignClient calling one microservices from other

--- inventory-services/src/main/java/com/codesushant/ecommerce/inventory_services/InventoryServicesApplication.java
+++ inventory-services/src/main/java/com/codesushant/ecommerce/inventory_services/InventoryServicesApplication.java
@@ -2,8 +2,10 @@ package com.codesushant.ecommerce.inventory_services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class InventoryServicesApplication {
 
 	public static void main(String[] args) {
